b055132e89ae64caecb1ce77b661feb5b3b1585f
Efendi Hariyadi <samman.alfariz@gmail.com>
2020-04-27 02:29:42 +0800

    🆕 Component SurahReading.vue config 🔖

---
+++ src/components/surah/SurahReading.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="parent" class="full-width row wrap justify-center items-start content-start" style="overflow: hidden;">
+    <div class="" style="overflow: auto;">
+      <q-card>
+        <q-card-section>
+          <img height="40px" alt="Al-Quraanul Kareem" src="~assets/images/surah_title_left.png">
+        </q-card-section>
+      </q-card>
+    </div>
+    <div class="" style="overflow: auto;">
+      <h3 style="line-height: 2px" class="surah-title">{{ surahName }}</h3>
+    </div>
+    <div class="" style="overflow: auto;">
+      <q-card>
+        <q-card-section>
+          <img height="40px" alt="Al-Quraanul Kareem" src="~assets/images/surah_title_right.png">
+        </q-card-section>
+      </q-card>
+    </div>
+    <div class="wrap col-12" style="">
+      <q-list v-for="item in surahData.ayahs" :key="item.numberInSurah" bordered>
+        <q-item>
+          <q-item-section>
+            <q-item-label>
+              <span>{{ item.numberInSurah }} </span>
+              <span style="float: right; direction: rtl" class="text-h5">
+                    {{ (item.numberInSurah === 1 && item.number !== 1 ? item.text.substr(39) : item.text) }}
+                      <q-avatar  size="24px" color="orange" text-color="black">
+                        {{ $StringUtils.numberToArabic(item.numberInSurah) }}
+                      </q-avatar>
+                  </span>
+            </q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SurahReadingComp',
+  props: {
+    surahNumber: {
+      required: true
+    },
+    surahName: {
+      type: String,
+      required: true,
+      default: ''
+    },
+    surahData: {
+      required: true
+    }
+  },
+  methods: {
+  }
+}
+</script>
+<style scoped>
+  .surah-title {
+    direction: rtl;
+    font-size: 35px;
+    font-family: 'me_quran',serif;
+    line-height: 1px;
+  }
+</style>
--- src/pages/SurahDetail.vue
+++ src/pages/SurahDetail.vue
@@ -1,49 +1,22 @@
 <template>
-  <!-- TODO create separate as component  -->
   <q-page padding class="flex flex-center column">
     <!-- content -->
     <div class="row" style="min-height: 400px; width: 100%;">
-      <div id="parent" class="full-width row wrap justify-center items-start content-start" style="overflow: hidden;">
-        <div class="" style="overflow: auto;">
-          <q-card>
-            <q-card-section>
-              <img height="40px" alt="Al-Quraanul Kareem" src="~assets/images/surah_title_left.png">
-            </q-card-section>
-          </q-card>
-        </div>
-        <div class="" style="overflow: auto;">
-          <h3 style="line-height: 2px" class="surah-title">{{ this.$store.state.QuranModul.selectSurah.name }}ِ</h3>
-        </div>
-        <div class="" style="overflow: auto;">
-        <q-card>
-          <q-card-section>
-            <img height="40px" alt="Al-Quraanul Kareem" src="~assets/images/surah_title_right.png">
-          </q-card-section>
-        </q-card>
-      </div>
-        <div class="wrap col-12" style="">
-          <q-list v-for="item in getSurah" :key="item.numberInSurah" bordered>
-            <q-item>
-              <q-item-section>
-                <q-item-label>
-                  <span>{{ item.numberInSurah }} </span>
-                  <span style="float: right; direction: rtl" class="text-h5">
-                    {{ (item.numberInSurah === 1 && item.number !== 1 ? item.text.substr(39) : item.text) }} {{ '('+ createArabicNumber(item.numberInSurah) + ')' }}
-                  </span>
-                </q-item-label>
-              </q-item-section>
-            </q-item>
-          </q-list>
-        </div>
-      </div>
+    <surah-reading-comp
+       :surah-data="this.$store.state.QuranModul.selectSurah"
+       :surah-name="this.$store.state.QuranModul.selectSurah.name"
+       :surah-number=this.$route.params.surah_number>
+    </surah-reading-comp>
     </div>
   </q-page>
 </template>
 
 <script>
-import NS from 'numeral-systems'
+// import NS from 'numeral-systems'
+import SurahReadingComp from 'components/surah/SurahReading'
 export default {
   name: 'SurahDetail',
+  components: { SurahReadingComp },
   data () {
     return {
       surah_number: this.$route.params.surah_number,
@@ -63,26 +36,16 @@ export default {
   },
   created () {
     this.initSurah()
-    console.log('NUMBER', this.createArabicNumber(10))
   },
   methods: {
     initSurah () {
       this.dataSelectSurah = {
         name: this.$store.state.QuranModul.selectSurah.name
       }
-    },
-    // TODO create utils
-    createArabicNumber (number) {
-      return NS(number, 'arabic')
     }
   }
 }
 </script>
 <style scoped>
-  .surah-title {
-    direction: rtl;
-    font-size: 35px;
-    font-family: 'me_quran',serif;
-    line-height: 1px;
-  }
+
 </style>
